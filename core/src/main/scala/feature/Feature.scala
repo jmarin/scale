2350ddcd44b09a755808dc5b1026896830590853
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-09-23 20:17:23 -0400

    Add projection of simple features

--- core/src/main/scala/feature/Feature.scala
+++ core/src/main/scala/feature/Feature.scala
@@ -1,30 +1,35 @@
 package feature
 
-import org.osgeo.proj4j.{ CRSFactory, CoordinateReferenceSystem }
+import com.vividsolutions.jts.{ geom => jts }
+import org.osgeo.proj4j.{
+  CRSFactory,
+  CoordinateReferenceSystem,
+  CoordinateTransformFactory,
+  CoordinateTransform,
+  ProjCoordinate
+}
 import geometry._
 
 object Feature {
 
+  lazy val crsFactory = new CRSFactory
+
   def apply[K, V](id: String, geometry: Geometry): Feature[String, Any] = {
-    val crsFactory = new CRSFactory
     val crs = crsFactory.createFromName("EPSG:4326")
     Feature(id, crs, geometry, Map.empty[String, Any])
   }
 
   def apply[K, V](id: String, srid: Int, geometry: Geometry): Feature[String, Any] = {
-    val crsFactory = new CRSFactory
     val crs = crsFactory.createFromName(s"EPSG:$srid")
     Feature(id, crs, geometry, Map.empty[String, Any])
   }
 
   def apply[K, V](id: String, geometry: Geometry, values: Map[K, V]): Feature[K, V] = {
-    val crsFactory = new CRSFactory
     val crs = crsFactory.createFromName("EPSG:4326")
     Feature(id, crs, geometry, values)
   }
 
   def apply[K, V](id: String, srid: Int, geometry: Geometry, values: Map[K, V]): Feature[K, V] = {
-    val crsFactory = new CRSFactory
     val crs = crsFactory.createFromName(s"EPSG:$srid")
     Feature(id, crs, geometry, values)
   }
@@ -36,6 +41,10 @@ object Feature {
  */
 case class Feature[K, V](id: String, crs: CoordinateReferenceSystem, geometry: Geometry, values: Map[K, V]) {
 
+  lazy val ctf = new CoordinateTransformFactory
+
+  lazy val crsFactory = new CRSFactory
+
   def countFields: Int = values.size
 
   def addOrUpdate(k: K, v: V): Feature[K, V] = Feature(id, crs, geometry, values.updated(k, v))
@@ -44,4 +53,68 @@ case class Feature[K, V](id: String, crs: CoordinateReferenceSystem, geometry: G
 
   def get(k: K): Option[V] = values.get(k)
 
+  def project(epsg: Int): Feature[K, V] = {
+    val outCRS = crsFactory.createFromName(s"EPSG:$epsg")
+    val transform = ctf.createTransform(crs, outCRS)
+    val projGeom = geometry.geometryType match {
+      case "Point" =>
+        projectPoint(transform, geometry.asInstanceOf[Point])
+      case "LineString" =>
+        projectLine(transform, geometry.asInstanceOf[Line])
+      case "Polygon" =>
+        projectPolygon(transform, geometry.asInstanceOf[Polygon])
+
+    }
+    Feature(id, outCRS, projGeom, values)
+  }
+
+  private def projectCoordinate(transform: CoordinateTransform, coord: jts.Coordinate): jts.Coordinate = {
+    val s = new ProjCoordinate(coord.x, coord.y)
+    val t = new ProjCoordinate
+    transform.transform(s, t)
+    new jts.Coordinate(t.x, t.y)
+  }
+
+  private def projectCoordinates(transform: CoordinateTransform, coords: Array[jts.Coordinate]): Array[jts.Coordinate] = {
+    coords.map(c => projectCoordinate(transform, c))
+  }
+
+  private def projectPoint(transform: CoordinateTransform, point: Point): Point = {
+    val c = point.jtsGeometry.getCoordinate
+    val pc = projectCoordinate(transform, c)
+    Point(pc.x, pc.y, srid(transform.getTargetCRS).toInt)
+  }
+
+  private def projectPoints(transform: CoordinateTransform, points: Array[Point]): Array[Point] = {
+    points.map(p => projectPoint(transform, p))
+  }
+
+  private def projectLine(transform: CoordinateTransform, line: Line): Line = {
+    val points = projectPoints(transform, line.points)
+    Line(points, srid(transform.getTargetCRS))
+  }
+
+  private def projectPolygon(transform: CoordinateTransform, polygon: Polygon): Polygon = {
+    val gf = polygon.jtsGeometry.getFactory
+    val exterior = gf.createLinearRing(
+      projectCoordinates(transform, polygon.jtsGeometry.getExteriorRing.getCoordinates))
+    val numHoles = polygon.jtsGeometry.getNumInteriorRing
+
+    def acc(list: List[jts.LineString], n: Int): List[jts.LineString] = n match {
+      case 0 => list
+      case _ =>
+        val hole = polygon.jtsGeometry.getInteriorRingN(n - 1)
+        acc(hole :: list, n - 1)
+    }
+
+    val rings = acc(Nil, numHoles)
+    val holes = rings.map { h =>
+      gf.createLinearRing(projectCoordinates(transform, h.getCoordinates))
+    }.toArray
+    Polygon(gf.createPolygon(exterior, holes))
+  }
+
+  private def srid(crs: CoordinateReferenceSystem): Int = {
+    crs.getName.substring(5, crs.getName.length).toInt
+  }
 }
--- core/src/test/scala/feature/FeatureSpec.scala
+++ core/src/test/scala/feature/FeatureSpec.scala
@@ -6,9 +6,23 @@ import geometry._
 class FeatureSpec extends Specification {
 
   val p1 = Point(-77, 39)
+  val p2 = Point(-76, 40)
+  val p3 = Point(-75, 38)
+  val p4 = Point(-77, 39)
+  val p6 = Point(-75.7, 39.2)
+  val p7 = Point(-76.5, 39)
+  val p8 = Point(-76, 38.5)
+  val line = Line(p1, p2)
+  val polygon = Polygon(p1, p2, p3, p4)
+  val boundary = Line(p1, p2, p3, p4)
+  val ring = Line(p6, p7, p8, p6)
+  val polyWithHoles = Polygon(boundary, ring)
   val id = "1"
   val values = Map("DESCRIPTION" -> "First Point")
   val f = Feature(id, p1, values)
+  val fl = Feature(id, line, values)
+  val fp = Feature(id, polygon, values)
+  val fph = Feature(id, polyWithHoles, values)
 
   "A Feature" should {
     "have some values" in {
@@ -38,7 +52,26 @@ class FeatureSpec extends Specification {
       val fn = f.updateGeometry(p2)
       fn.geometry must be equalTo (p2)
     }
-
+    "project a Point to another CRS" in {
+      val p = f.project(3857)
+      p.crs.getName must be equalTo ("EPSG:3857")
+      p.geometry.wkt must be equalTo ("POINT (-8571600.791082066 4721671.572580107)")
+    }
+    "project a Line to another CRS" in {
+      val p = fl.project(3857)
+      p.crs.getName must be equalTo ("EPSG:3857")
+      p.geometry.wkt must be equalTo ("LINESTRING (-8571600.791082066 4721671.572580107, -8460281.300288793 4865942.279503176)")
+    }
+    "project a Polygon to another CRS" in {
+      val p = fp.project(3857)
+      p.crs.getName must be equalTo ("EPSG:3857")
+      p.geometry.wkt must be equalTo ("POLYGON ((-8571600.791082066 4721671.572580107, -8460281.300288793 4865942.279503176, -8348961.809495518 4579425.812870097, -8571600.791082066 4721671.572580107))")
+    }
+    "project a Polygon with holes to another CRS" in {
+      val p = fph.project(3857)
+      p.crs.getName must be equalTo ("EPSG:3857")
+      p.geometry.wkt must be equalTo ("POLYGON ((-8571600.791082066 4721671.572580107, -8460281.300288793 4865942.279503176, -8348961.809495518 4579425.812870097, -8571600.791082066 4721671.572580107), (-8426885.453050809 4750360.4811166, -8515941.045685427 4721671.572580107, -8460281.300288793 4650301.836738959, -8426885.453050809 4750360.4811166))")
+    }
   }
 
 }
