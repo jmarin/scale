0d830739a7449dd915d9c777b5b1256235033c1c
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-10-02 20:36:47 -0400

    Add Line GeoJSON serialization

--- serialization/src/main/scala/geojson/GeoJsonProtocol.scala
+++ serialization/src/main/scala/geojson/GeoJsonProtocol.scala
@@ -1,22 +1,50 @@
 package geojson
 
 import spray.json._
-import geometry.Point
+import geometry._
 import feature._
 
 object GeoJsonProtocol extends DefaultJsonProtocol with NullOptions {
 
   implicit object PointFormat extends JsonFormat[Point] {
-    def write(p: Point): JsValue =
+    def write(p: Point): JsValue = {
       JsObject(
         "type" -> JsString("Point"),
         "coordinates" -> JsArray(JsNumber(p.x), JsNumber(p.y), JsNumber(p.z)))
+    }
 
-    def read(j: JsValue): Point = {
-      j.asJsObject.getFields("coordinates") match {
-        case Seq(JsArray(Vector(JsNumber(x), JsNumber(y), JsNumber(z)))) =>
+    def read(json: JsValue): Point = {
+      json.asJsObject.getFields("type", "coordinates") match {
+        case Seq(JsString(t), JsArray(Vector(JsNumber(x), JsNumber(y), JsNumber(z)))) =>
           Point(x.toDouble, y.toDouble, z.toDouble)
         case _ => throw new DeserializationException("Point GeoJSON expected")
+      }
+    }
+  }
+
+  implicit object LineFormat extends JsonFormat[Line] {
+    def write(l: Line): JsValue = {
+      val coords = l.coordinates.map { c =>
+        JsArray(JsNumber(c.x), JsNumber(c.y), JsNumber(c.z))
+      }.toVector
+      JsObject(
+        "type" -> JsString("LineString"),
+        "coordinates" -> JsArray(coords)
+      )
+    }
+
+    def read(json: JsValue): Line = {
+      json.asJsObject.getFields("type", "coordinates") match {
+        case Seq(JsString(t), JsArray(p)) =>
+          val points = p.map { x =>
+            val point = JsObject(
+              "type" -> JsString("Point"),
+              "coordinates" -> x
+            )
+            point.convertTo[Point]
+          }
+          Line(points)
+        case _ => throw new DeserializationException("LineString GeoJSON expected")
       }
     }
   }
--- serialization/src/test/scala/geojson/JsonProtocolSpec.scala
+++ serialization/src/test/scala/geojson/JsonProtocolSpec.scala
@@ -9,15 +9,24 @@ class GeoJsonProtocolSpecification extends Specification {
 
   import geojson.GeoJsonProtocol._
 
-  val point = Point(-77.1, 38.5)
+  val p1 = Point(-77.1, 38.5)
+  val p2 = Point(-102.2, 45.8)
+  val l1 = Line(p1, p2)
   val pointJson = """{"type":"Point","coordinates":[-77.1,38.5,0.0]}"""
+  val lineJson = """{"type":"LineString","coordinates":[[-77.1,38.5,0.0],[-102.2,45.8,0.0]]}"""
 
   "The JSON protocol" should {
     "serialize Point to GeoJSON" in {
-      point.toJson.toString must be equalTo (pointJson)
+      p1.toJson.toString must be equalTo (pointJson)
     }
     "read GeoJSON into Point" in {
-      point.toJson.convertTo[Point] must be equalTo (point)
+      p1.toJson.convertTo[Point] must be equalTo (p1)
+    }
+    "serialize Line to GeoJSON" in {
+      l1.toJson.toString must be equalTo (lineJson)
+    }
+    "read GeoJSON into Line" in {
+      l1.toJson.convertTo[Line] must be equalTo (l1)
     }
   }
 }
