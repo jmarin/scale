b87011b52da6de3c5af0a52b2e668319a8818083
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-03-04 18:51:35 -0500

    Trying some STRtree stuff

--- core/src/main/scala/feature/FeatureCollection.scala
+++ core/src/main/scala/feature/FeatureCollection.scala
@@ -4,6 +4,9 @@ import com.vividsolutions.jts.{ geom => jts }
 import geometry._
 import feature._
 
+import java.util.Calendar
+import com.vividsolutions.jts.index.strtree.STRtree
+
 object FeatureCollection {
 
   def apply(features: Feature*): FeatureCollection = {
@@ -23,11 +26,36 @@ case class FeatureCollection(features: Traversable[Feature]) {
   }
 
   def pointInPoly(p: Point): Option[Traversable[Feature]] = {
+
+    val start = Calendar.getInstance().getTimeInMillis()
+
     features.foreach { f =>
       val geomType = f.geometry.geometryType
       require(geomType == "Polygon" || geomType == "MultiPolygon")
     }
+
     val polys = features.filter(f => f.geometry.contains(p))
+
+    val end = Calendar.getInstance().getTimeInMillis()
+
+    val time = end - start
+    println("POINT IN POLY PROCESSING TIME: " + time + "ms")
+
+    val start2 = Calendar.getInstance().getTimeInMillis()
+
+    val strTree = new STRtree()
+    features.foreach { f =>
+      strTree.insert(f.geometry.jtsGeometry.asInstanceOf[jts.Polygon].getEnvelopeInternal, f.geometry.jtsGeometry)
+    }
+
+    strTree.build()
+
+    val list = strTree.query(p.jtsGeometry.getEnvelopeInternal)
+
+    val end2 = Calendar.getInstance().getTimeInMillis()
+
+    println("POINT IN POLY STRTREE PROCESSING TIME: " + time + "ms")
+
     if (polys.size > 0) Some(polys) else None
   }
 }
--- core/src/main/scala/geometry/Polygon.scala
+++ core/src/main/scala/geometry/Polygon.scala
@@ -14,14 +14,14 @@ object Polygon {
 
   def apply(points: Traversable[Point]): Polygon = {
     val ring = geomFactory.createLinearRing(Util.points2JTSCoordinates(points).toArray)
-    require(ring.isValid)
+    //require(ring.isValid)
     Polygon(new jts.Polygon(ring, null, geomFactory))
   }
 
   def apply(points: Traversable[Point], srid: Int): Polygon = {
     val gf = new jts.GeometryFactory(null, srid)
     val ring = geomFactory.createLinearRing(Util.points2JTSCoordinates(points).toArray)
-    require(ring.isValid)
+    //require(ring.isValid)
     Polygon(new jts.Polygon(ring, null, gf))
   }
 
@@ -35,22 +35,26 @@ object Polygon {
 
   def apply(exterior: Line, interior: Array[Line]): Polygon = {
     val exteriorRing = geomFactory.createLinearRing(
-      Util.points2JTSCoordinates(exterior.points).toArray)
-    require(exteriorRing.isValid)
+      Util.points2JTSCoordinates(exterior.points).toArray
+    )
+    //require(exteriorRing.isValid)
     val holes = interior.map(i => geomFactory.createLinearRing(
-      Util.points2JTSCoordinates(i.points).toArray))
-    holes.foreach(h => require(h.isValid))
+      Util.points2JTSCoordinates(i.points).toArray
+    ))
+    //holes.foreach(h => require(h.isValid))
     Polygon(new jts.Polygon(exteriorRing, holes, geomFactory))
   }
 
   def apply(exterior: Line, interior: Array[Line], srid: Int): Polygon = {
     val gf = new jts.GeometryFactory(null, srid)
     val exteriorRing = gf.createLinearRing(
-      Util.points2JTSCoordinates(exterior.points).toArray)
-    require(exteriorRing.isValid)
+      Util.points2JTSCoordinates(exterior.points).toArray
+    )
+    //require(exteriorRing.isValid)
     val holes = interior.map(i => gf.createLinearRing(
-      Util.points2JTSCoordinates(i.points).toArray))
-    holes.foreach(h => require(h.isValid))
+      Util.points2JTSCoordinates(i.points).toArray
+    ))
+    //    holes.foreach(h => require(h.isValid))
     Polygon(new jts.Polygon(exteriorRing, holes, gf))
   }
 
